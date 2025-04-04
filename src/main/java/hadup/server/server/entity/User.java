1e89083394dfa75e83c966aea117ba7514cb1932
ntnhtc <han.ntnhtc@gmail.com>
2025-04-04 21:19:28 +0700

    add email property for User to fix error creating bean with name 'userRepository'

--- src/main/java/hadup/server/server/entity/User.java
+++ src/main/java/hadup/server/server/entity/User.java
@@ -18,6 +18,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+    private String email;
     private String username;
     private String password;
 
